8cefca4e7ad82d2ac67d33465196b76e53011ff5
krk224 <hihas93@naver.com>
2021-05-05 22:08:50 +0900

    20210505_Study_21-22시

---
+++ web/FastCampus/JavaScript/hoisting_ex1.js
@@ -0,0 +1,70 @@
+// 함수 정의 먼저
+
+function hello1(){
+    console.log("hello1");
+}
+
+hello1();
+
+// 함수 호출 먼저
+
+hello2();
+
+function hello2(){
+    console.log("hello2");
+}
+
+// 문제 없슴!
+
+// example2.js
+console.log("Hoisting Example2");
+
+age = 6;
+age++;
+console.log("age 먼저 호출한 값 : " + age);
+
+var age = 5;
+
+//example3.js
+
+console.log('\nHoisting이 선언만을 가져온다는 것을 보여주는 예시')
+
+console.log(name);
+
+name = "Mark";
+
+console.log(name);
+
+var name = "Chris";
+
+// 아래에서 할당을 해준다고 해도, 위로는 선언만이 올라가고, 할당은 맨 마지막에 남는다!!!
+
+/*
+    var name; // 선언만 올라가고,
+    console.log(name);
+    name = "Mark";
+    console.log(name);
+    name = "Chris"; // 할당은 남는다.
+
+    와 동일 => var 변수 형의 문제점!!
+
+*/
+
+
+//===============================================
+
+// let을 사용할 때의 Hoisting은??
+
+/* 
+    아예 에러 처리됨 => Hoisting 문제 해결됨.
+    
+    console.log('letName');
+
+    letName = "KKR";
+
+    console.log(letName);
+
+    let letName;
+
+*/
+
---
+++ web/FastCampus/JavaScript/var함수스코프_예제.js
@@ -0,0 +1,42 @@
+// var 함수 스코프
+
+var a = 0;
+console.log(a);
+
+(function(){
+    a++;
+    console.log(a);
+})(); // 함수 즉시 실행 방법.
+
+console.log(a);
+
+(function(){
+    var b = 0;
+    console.log(b);
+})();
+
+// b++; => 함수를 벗어나 에러 발생.
+
+
+//var를 블록 스코프에서 사용.
+
+// main? 함수에서 사용해서 블록 안까지 괜춘.
+var c =0;
+
+{
+    c++;
+    console.log(c);
+}
+
+
+// 블록 안에서 바깥으로 사용. => (ok)
+
+{
+    var d =0;
+    console.log(d);
+}
+
+// 블록과 상관없기 때문에 에러 발생하지 않는다.
+console.log(d);
+
+
