3f6c22f8ceeef9cd13b97a582704a354ccaed09f
KRyun <hihas93@naver.com>
2021-08-03 17:09:36 +0900

    다듬기

--- web/Dream Coding/YouTube_Clone/mainjs.js
+++ web/Dream Coding/YouTube_Clone/mainjs.js
@@ -1,9 +1,40 @@
 window.onload = function() {
-  const likeIcon = document.querySelector('fas fa-thumbs-up');
-  const disLikeIcon = document.querySelector ('fas fa-thumbs-down');
+  const likeBtn = document.querySelector('.liked');
+  const dislikeBtn = document.querySelector ('.disliked');
+  const thumbsIconsSpan = document.querySelector('.thumbsIcons');
 
+  const onClick = e =>{
+    console.log('isworking');
+    if(e.target.classList.contains('liked')){
+      console.log('liked clicked');
+      likeBtn.classList.toggle('active');
+      if(dislikeBtn.classList.contains('active')){
+        console.log('disliked toggle');
+        dislikeBtn.classList.toggle('active');
+        
+      }
+        
+    }
+    else if(e.target.classList.contains('disliked')) {
+      console.log('disliked clicked');
+      dislikeBtn.classList.toggle('active');
+      if(likeBtn.classList.contains('active'))
+      {
+        console.log('liked toggle');
+        likeBtn.classList.toggle('active');
+      }
+      
+    }
+
+  }
+
+  thumbsIconsSpan.addEventListener('click', onClick)
+  
   
 }
+
+
+
 
 
 
