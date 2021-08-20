64701a8c59923935549319944c7f7598e67672a8
KRyun <hihas93@naver.com>
2021-08-20 23:50:34 +0900

    반응형 search bar 추가, js code 수정, hover icon 생성

--- web/Dream Coding/YouTube_Clone/mainjs.js
+++ web/Dream Coding/YouTube_Clone/mainjs.js
@@ -5,7 +5,9 @@ window.onload = function() {
 
   const onClick = e =>{
     console.log('isworking');
-    if(e.target.classList.contains('liked')){
+    console.log('e.target: ' +e.target);
+    console.log('icon cliked?: '+e.target.classList.contains('fas'))
+    if(e.target.classList.contains('fa-thumbs-up') || e.target.parentElement.classList.contains('liked')){
       console.log('liked clicked');
       likeBtn.classList.toggle('active');
       if(dislikeBtn.classList.contains('active')){
@@ -15,11 +17,20 @@ window.onload = function() {
       }
         
     }
-    else if(e.target.classList.contains('disliked')) {
+    else if(e.target.classList.contains('fa-thumbs-down') || e.target.parentElement.classList.contains('disliked')) {
       console.log('disliked clicked');
       dislikeBtn.classList.toggle('active');
+      console.log(dislikeBtn.children[0]);
+      console.log(Number(dislikeBtn.children[0].textContent));
+      if(Number(dislikeBtn.children[0].textContent)=== 28){
+        dislikeBtn.children[0].innerHTML = '<i class="fas fa-thumbs-down"></i> 29';
+      }
+      else {
+        dislikeBtn.children[0].innerHTML = '<i class="fas fa-thumbs-down"></i> 28';
+      }
       if(likeBtn.classList.contains('active'))
       {
+
         console.log('liked toggle');
         likeBtn.classList.toggle('active');
       }
